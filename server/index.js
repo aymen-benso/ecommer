0c01d3827ab143d57f747978b1995d6a8c68e8dc
aymen benso <117603473+aymen-benso@users.noreply.github.com>
2023-09-28 14:10:10 +0000

    done

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+// Serve static files from the client folder
+app.use(express.static(path.join(__dirname, "../client/build/")));
+
+// Serve index.html when the root URL is requested
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/", "index.html"));
+});
+
 app.get("/api", (req, res) => {
-    res.json({ products:["product 1","product 2","product 3"] });
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  res.json({users: [
+    {name: "John Doe"},
+    {name: "Jane Doe"},
+    {name: "Joe Doe"},
+  ]});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
