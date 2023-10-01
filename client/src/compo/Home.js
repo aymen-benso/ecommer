cdc541022c4f019572968f11a797c8fb5f201507
aymen benso <117603473+aymen-benso@users.noreply.github.com>
2023-10-01 00:04:50 +0000

    add categories to home page

--- client/src/compo/Home.js
+++ client/src/compo/Home.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from 'react';
-
+import './Home.css';
 const Home = () => {
-        const [BackendData, SetBackendData] = useState([]);
-        const [isLoading, setIsLoading] = useState(true);
-      
-        useEffect(() => {
-          fetch('/api').then(
-            response => response.json()
-          ).then(
-            data => {
-              SetBackendData(data.users);
-              console.log(data); // Log the value of BackendData to the console
-              setIsLoading(false);
-            }
-          )
-        }, [])
-      
-    return (
-        <div className="home">
-            <h1>Home Page</h1>
-            {isLoading? (
-          <p>Loading...</p>
-        ) : (
-          <ul>
-            {Array.isArray(BackendData)? (
-              BackendData.map((user, index) => (
-                <li key={index}>{user.name}</li>
-              ))
-            ) : (
-              <p>BackendData is not an array</p>
-            )}
-          </ul>
-        )}
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/category').then(
+      response => response.json()
+    ).then(
+      data => {
+        setCategory(data.categories);
+        console.log(data); // Log the value of BackendData to the console
+      }
+    )
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="big-image">
+        <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1200/https://blog.snappa.com/wp-content/uploads/2019/08/header-images.jpg" alt="Big Image" />
+      </div>
+      {Array.isArray(category) && category.length > 0 && (
+        <div className="category">
+          <h2>Categories</h2>
+          <div className="category-icons">
+            {category.map((item) => (
+              <div className="category-icon" key={item.id}>
+                <img src={item.icon} alt={item.name} />
+                <span>{item.name}</span>
+              </div>
+            ))}
+          </div>
         </div>
-    ); 
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Serve static files from the client folder
 app.use(express.static(path.join(__dirname, "../client/build/")));
@@ -19,6 +23,39 @@ app.get("/api", (req, res) => {
     {name: "Jane Doe"},
     {name: "Joe Doe"},
   ]});
+});
+
+app.get("/api/category", (req, res) => {
+  res.json(
+    {
+  "categories": [
+    {
+      "id": 1,
+      "icon": "https://example.com/icons/category1.png",
+      "name": "Category 1"
+    },
+    {
+      "id": 2,
+      "icon": "https://example.com/icons/category2.png",
+      "name": "Category 2"
+    },
+    {
+      "id": 3,
+      "icon": "https://example.com/icons/category3.png",
+      "name": "Category 3"
+    },
+    {
+      "id": 4,
+      "icon": "https://example.com/icons/category4.png",
+      "name": "Category 4"
+    },
+    {
+      "id": 5,
+      "icon": "https://example.com/icons/category5.png",
+      "name": "Category 5"
+    }
+  ]
+});
 });
 
 app.listen(PORT, () => {
